d456211dd55cefcef398c53719c5e5231adc5bd7
NastyaRebrova <rebrrova.nastya@gmail.com>
2025-09-13 17:51:08 +0300

    сделана лаб2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('main.article');
-});
+Route::get('/', [MainController::class, 'index']);
+Route::get('/full_image/{img}', [MainController::class, 'show']);
 
 Route::get('/about', function () {
     return view('main.about');
