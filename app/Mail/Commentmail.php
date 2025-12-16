3bc79b3e5abb67b4c3577e120dde264d4550d417
NastyaRebrova <rebrrova.nastya@gmail.com>
2025-12-16 16:20:45 +0300

    сделана лаб8

---
+++ app/Mail/Commentmail.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Comment;
+use App\Models\Article;
+
+class Commentmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(public Comment $comment, public Article $article)
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            from: new Address(env("MAIL_FROM_ADDRESS"), env("MAIL_FROM_NAME")),
+            subject: 'Commentmail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'mail.comment',
+            with: [
+                'article_title'=>$this->article->title,
+                'comment'=>$this->comment,
+                'author'=>auth()->user()->name,
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
